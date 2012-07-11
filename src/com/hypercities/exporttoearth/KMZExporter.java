935000c7a3edaebd17599d754fed3526c68c6077
Dave Shepard <shepard.david@gmail.com>
2012-07-11 11:24:57 -0700

    Added license to source files.

--- src/com/hypercities/exporttoearth/KMZExporter.java
+++ src/com/hypercities/exporttoearth/KMZExporter.java
@@ -1,3 +1,28 @@
+/**
+ * Copyright (c) 2012, David Shepard 
+ * All rights reserved.  
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met: 
+ *  1. Redistributions of source code must retain the above copyright notice, 
+ *      this list of conditions and the following disclaimer. 
+ *  2. Redistributions in binary form must reproduce the above copyright notice, 
+ *      this list of conditions and the following disclaimer in the 
+ *      documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.  
+ * 
+ */
 package com.hypercities.exporttoearth;
 
 import de.micromata.opengis.kml.v_2_2_0.*;
@@ -263,15 +288,16 @@ public class KMZExporter implements GraphExporter, ByteExporter, LongTask {
 		}
 		try {
 			writeKMZ(kml, renderer);
+            JOptionPane.showMessageDialog(null, "Export complete",
+				"KML Export complete.", JOptionPane.INFORMATION_MESSAGE);
 		} catch (IOException ex) {
 			Logger.getLogger(KMZExporter.class.getName()).log(Level.SEVERE, null, ex);
-		}
-
-
-		ticket.finish();
-		JOptionPane.showMessageDialog(null, "Export complete",
-				"KML Export complete.", JOptionPane.INFORMATION_MESSAGE);
-		return true;
+            JOptionPane.showMessageDialog(null, "Error saving.", 
+                    "Sorry, could not write the file.", JOptionPane.ERROR_MESSAGE);
+		} finally {
+            ticket.finish();
+            return true;
+        }
 	}
 
 	private String getAttributeField(String[] patterns, AttributeModel model) {
@@ -321,7 +347,6 @@ public class KMZExporter implements GraphExporter, ByteExporter, LongTask {
 
 	@Override
 	public void setProgressTicket(ProgressTicket pt) {
-		Logger.getGlobal().log(Level.INFO, "Progres ticket acquired");
 		ticket = pt;
 	}
 
